756ec8377425857f0f6359d2f23eb97f34e602fc
Dust1920 <a218208885@gmail.com>
2024-03-25 15:01:39 -0700

    w

--- mapper_addons.py
+++ mapper_addons.py
@@ -1,6 +1,20 @@
 import general_codes as gc
+import os
 import pandas as pd
 # Flechas, Texto, Ejes
+
+
+def call_template(region, template):
+    files = os.listdir('geodata\\Mexico')
+    for f in files:
+        try:
+            f.index(region)
+            id = f[:2]
+        except:
+            continue
+    temp_df = pd.read_excel(f'templates\\{id}.xlsx', sheet_name=template, index_col=0)
+    return temp_df
+
 
 def legend_df(xs):
     u = xs[['legend_colors','legend_names']]
@@ -47,41 +61,6 @@ def arrow_block(content, c_flecha, c_text, config, ax, **kwargs):
         }
     ax.annotate(content, xy= c_flecha, xytext= c_text, arrowprops = arrow_cfig,
                 fontsize = fs, color = fc, weight = fw)
-
-def read_template2(map, template, ax, **kwargs):
-    color_t = template['color_relleno']
-    map.plot(color = color_t, ax = ax)
-    ib_0 = list(template.index)[0]
-    title = template.loc[ib_0,'title']
-    title_config = gc.text_to_list(template.loc[ib_0,'title_conf'])
-    fw = title_config[2] if title_config[2]!= 'None' else None
-    ax.set_title(title,
-                 fontsize = title_config[0], color = title_config[1], weight = fw)
-    ac_bd = template.loc[ib_0,'bd']
-    for reg in template.index:
-        t_x = template.loc[reg, 'texto_x']
-        t_y = template.loc[reg, 'texto_y']
-        f_x = template.loc[reg, 'flecha_x']
-        f_y = template.loc[reg, 'flecha_y']
-        t_coords = (t_x, t_y)
-        f_coords = (f_x, f_y)
-        flecha = template.loc[reg, 'flecha']
-        text_cfig = template.loc[reg, 'text_conf']
-        text_cfig = gc.text_to_list(text_cfig)
-        if flecha:
-            flecha_sets = template.loc[reg, 'flecha_conf']
-            flecha_sets = gc.text_to_list(flecha_sets, ntype = float)
-            arrow_block(template.loc[reg,'texto'], f_coords, t_coords, flecha_sets, ax,
-                        text_cf = text_cfig)
-        else:
-            text_block(template.loc[reg,'texto'], t_coords, ax,
-                       text_cf = text_cfig)
-    if int(ac_bd[0]):
-        bd = gc.text_to_list(template.loc[ib_0,'bd'])
-        map.boundary.plot(color = bd[1], lw = bd[2], ax = ax)
-    if not template.loc[ib_0, 'axis']:
-        ax.set_axis_off()
-    return 0
 
 
 def read_template(map, template, ax, **kwargs):
